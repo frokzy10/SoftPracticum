927c233cddf025761dd7b40542bdcce2b1b1a68c
Nurdin Bakytbekov <kmeks81@gmail.com>
2024-04-12 14:53:52 +0600

    nurdin :I add FSD

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import cls from "./Header.module.scss"
+const Header = () => {
+    return (
+        <header className={cls.header}>
+            Header
+        </header>
+    );
+};
+
+export default Header;
